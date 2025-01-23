43bc5669f2a08c39413dc1ade7462b6e89562928
VitorSoer <contato.soer@gmail.com>
2025-01-23 16:48:20 -0300

    ✨ feat: Add components index export.

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// Buttons
+export { default as Button } from './components/Buttons/Button';
+export { default as Checkbox } from './components/Buttons/Checkbox';
+export { default as Radio } from './components/Buttons/Radio';
+export { default as Switch } from './components/Buttons/Switch';
+
+// Feedback
+export { default as Avatar } from './components/Feedback/Avatar';
+export { default as Icon } from './components/Feedback/Icon';
+export { default as PieChart } from './components/Feedback/PieChart';
+export { default as ProgressCircle } from './components/Feedback/ProgressCircle';
+export { default as Rating } from './components/Feedback/Rating';
+export { default as Skeleton } from './components/Feedback/Skeleton';
+export { default as Spinner } from './components/Feedback/Spinner';
+export { default as Toast } from './components/Feedback/Toast';
+export { default as Tooltip } from './components/Feedback/Tooltip';
+
+// Inputs
+export { default as Range } from './components/Inputs/Range';
+
+// Navigation
+export { default as Pagination } from './components/Navigation/Pagination';
+
+// TextElements
+export { default as TextDisplay } from './components/TextElements/TextDisplay';
+
+// Provider
+export { StyledThemeProvider } from './theme/providers';
